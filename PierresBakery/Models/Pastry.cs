05781302a62cf570b27a36e3c9350a703063afb1
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-12-10 13:46:14 -0900

    red test for CalculateTotal

--- PierresBakery/Models/Pastry.cs
+++ PierresBakery/Models/Pastry.cs
@@ -10,5 +10,11 @@ namespace Bakery.Models
     {
       _pastryOrder.Add(this);
     }
+    new public static int CalculateTotal()  //While we can't override static methods, we can hide them. The new keyword here will hide the parent objects CalculateTotal method and use this one instead.
+    {
+      int total = 0;
+      
+      return total;
+    }
   }
 }
