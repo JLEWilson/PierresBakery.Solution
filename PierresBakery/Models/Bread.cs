a88fa5b072ecd9c4345c8ed1c749e0d1bbc5d404
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-12-10 10:19:04 -0900

    red test for Bread.ApplyDiscount

--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -1,10 +1,20 @@
+using System.Collections.Generic;
+
 namespace Bakery.Models
 {
   public class Bread : FoodItem
   {
+    private static List<Bread> _breadOrder = new List<Bread> {};
+    private int _breadOrderTotalPrice;
     public Bread(int price) : base(price)
     {
-      
+      _breadOrder.Add(this);
+      _breadOrderTotalPrice += this.GetPrice();
+    }
+
+    public static int ApplyDiscount(int itemsNeededForSale, int priceReduction) //This is separate from FoodItem.ApplyDiscount because bread objects don't inherit the static fields or methods
+    {
+      return 0;
     }
   }
 }
