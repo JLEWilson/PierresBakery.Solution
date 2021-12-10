eb137e4ef9ee5bc49cb3785e69d56a2fc883752e
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-12-10 11:59:51 -0900

    update constructors for new parameter

--- PierresBakery.Tests/ModelTests/FoodItemTests.cs
+++ PierresBakery.Tests/ModelTests/FoodItemTests.cs
@@ -15,25 +15,25 @@ namespace Bakery.Tests
     [TestMethod]
     public void FoodItemConstructor_CreateInstanceOfFoodItem_FoodItem()
     {
-      FoodItem newFoodItem = new FoodItem(3);
+      FoodItem newFoodItem = new FoodItem(3, "Generic");
       Assert.AreEqual(typeof(FoodItem), newFoodItem.GetType());
     }
 
     [TestMethod]
     public void GetPrice_ReturnPriceOfFoodItem_5()
     {
-      FoodItem newFoodItem = new FoodItem(5);
+      FoodItem newFoodItem = new FoodItem(5, "Generic");
       int price = newFoodItem.GetPrice();
       Assert.AreEqual(5, price);
     }
     [TestMethod]
     public void ApplyDiscount_ReturnPriceOfAllItemsWithDiscount_20()
     {
-      FoodItem newFoodItem1 = new FoodItem(5);
-      FoodItem newFoodItem2 = new FoodItem(5);
-      FoodItem newFoodItem3 = new FoodItem(5);
-      FoodItem newFoodItem4 = new FoodItem(5);
-      FoodItem newFoodItem5 = new FoodItem(5);
+      FoodItem newFoodItem1 = new FoodItem(5, "Generic");
+      FoodItem newFoodItem2 = new FoodItem(5, "Generic");
+      FoodItem newFoodItem3 = new FoodItem(5, "Generic");
+      FoodItem newFoodItem4 = new FoodItem(5, "Generic");
+      FoodItem newFoodItem5 = new FoodItem(5, "Generic");
       int total = FoodItem.ApplyDiscount(5, 5); // Should reduce total by 5 for every 5 items ordered
       Assert.AreEqual(20, total);
     }
