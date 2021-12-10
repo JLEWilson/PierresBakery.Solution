8698a1f931708f7046e096a98cec26d8ae7b8a84
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-12-10 13:46:14 -0900

    red test for CalculateTotal

--- PierresBakery.Tests/ModelTests/PastryTests.cs
+++ PierresBakery.Tests/ModelTests/PastryTests.cs
@@ -18,5 +18,14 @@ namespace Bakery.Tests
       Pastry newPastry = new Pastry(3);
       Assert.AreEqual(typeof(Pastry), newPastry.GetType());
     }
+    [TestMethod]
+    public void CalculateTotal_ReturnPriceOfAllPastrys_15()
+    {
+      Pastry newPastry = new Pastry(5);
+      Pastry newPastry1 = new Pastry(5);
+      Pastry newPastry2 = new Pastry(5);
+      int total = Pastry.CalculateTotal();
+      Assert.AreEqual(15, total);
+    }
   }
 }
