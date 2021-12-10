e0831c5b0c1d3779b56f64e067f630893830fd0f
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-12-10 10:19:04 -0900

    red test for Bread.ApplyDiscount

--- PierresBakery.Tests/ModelTests/BreadTests.cs
+++ PierresBakery.Tests/ModelTests/BreadTests.cs
@@ -20,11 +20,23 @@ namespace Bakery.Tests
     }
 
     [TestMethod]
-    public void BreadConstructor_InheritPricePropertyAndGetMethodFromFoodItem_3()
+    public void BreadConstructor_InheritPricePropertyAndGetMethodFromBread_3()
     {
       Bread newBread = new Bread(3);
       int price = newBread.GetPrice();
       Assert.AreEqual(3, price);
+    }
+
+    [TestMethod]
+    public void ApplyDiscount_ReturnPriceOfAllItemsWithDiscount_20()    
+    {
+      Bread newBread1 = new Bread(5);
+      Bread newBread2 = new Bread(5);
+      Bread newBread3 = new Bread(5);
+      Bread newBread4 = new Bread(5);
+      Bread newBread5 = new Bread(5);
+      int total = Bread.ApplyDiscount(3, 5); // Should reduce total by 5 for every 3 items ordered
+      Assert.AreEqual(20, total);
     }
   }
 }
