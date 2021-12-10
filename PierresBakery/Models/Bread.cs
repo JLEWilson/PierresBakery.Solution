5e415cb2db8d17afb99fe4613234dff4c6551cd6
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-12-10 14:06:11 -0900

    add syntax fixes

--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -12,7 +12,7 @@ namespace Bakery.Models
       _breadOrder.Add(this);
     }
 
-    new public static int CalculateTotal()  //While we can't override static methods, we can hide them. The new keyword here will hide the parent objects ApplyDiscount method and use this one instead.
+    new public static int CalculateTotal()  //While we can't override static methods, we can hide them. The new keyword here will hide the parent objects CalculateTotal method and use this one instead.
     {
       int total = 0;
       foreach(Bread item in _breadOrder)
