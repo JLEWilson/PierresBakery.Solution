aa415e4f54c7ff69d3cff29bdfd57d801cc171fe
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-12-10 13:27:51 -0900

    green test for CalculateTotal, fixes ApplyDiscount

--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -15,6 +15,10 @@ namespace Bakery.Models
     new public static int CalculateTotal()  //While we can't override static methods, we can hide them. The new keyword here will hide the parent objects ApplyDiscount method and use this one instead.
     {
       int total = 0;
+      foreach(Bread item in _breadOrder)
+      {
+        total += item.GetPrice();
+      }
       return total;
     }
     new public static int ApplyDiscount(int itemsNeededForSale, int priceReduction) 
