c7ea50e7981d82062c2abd6cf477a7e375937f8b
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-12-10 13:55:02 -0900

    add green test for ClearPastryFromOrder(), functionality was needed for another test

--- PierresBakery.Tests/ModelTests/PastryTests.cs
+++ PierresBakery.Tests/ModelTests/PastryTests.cs
@@ -13,6 +13,21 @@ namespace Bakery.Tests
       Pastry.ClearPastryFromOrder();
     }
     [TestMethod]
+    public void ClearPastryFromOrder_ClearPastryItemsAndRemoveThemFromFoodItemOrder_2()
+    {
+      //add 4 items to FoodItem order
+      Bread newBread1 = new Bread(5);
+      Bread newBread2 = new Bread(5);
+      Pastry newPastry1 = new Pastry(3);
+      Pastry newPastry2 = new Pastry(3);
+      //remove Pastry from both the FoodItem order and Pastry order
+      Pastry.ClearPastryFromOrder();
+      //get remaining count of foodItem _order
+      FoodItem.GetOrder();
+      int itemCount = FoodItem.GetOrder().Count;
+      Assert.AreEqual(2, itemCount);
+    }
+    [TestMethod]
     public void PastryConstructor_CreateInstanceOfPastry_Pastry()
     {
       Pastry newPastry = new Pastry(3);
