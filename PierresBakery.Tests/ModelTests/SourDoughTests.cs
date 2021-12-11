c4ac747423a20953b179fb08572bc76fb6279767
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-12-10 15:34:53 -0900

    fix wrong class declaration

--- PierresBakery.Tests/ModelTests/SourDoughTests.cs
+++ PierresBakery.Tests/ModelTests/SourDoughTests.cs
@@ -1,10 +1,10 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Bakery.Models;
 
-namespace Sourdough.Tests
+namespace Bakery.Tests
 {
   [TestClass]
-  public class PastryTests
+  public class SourdoughTests
   {
     [TestMethod]
     public void SourDoughConstructor_CreateInstanceOfSourDough_Sourdough()
