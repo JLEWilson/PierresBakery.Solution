2da46518f6c35b174d1f5742c7e69e6c0e6ecca6
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-12-12 11:55:23 -0900

    price adjustments

--- PierresBakery/Models/Pastries/Macaron.cs
+++ PierresBakery/Models/Pastries/Macaron.cs
@@ -4,8 +4,8 @@ namespace Bakery.Models
 {
   public class Macaron : Pastry
   {
-    public static int Cost {get;set;} = 4;
-    public static string Name {get;set;} = "Croissant";
+    public static int Cost {get;set;} = 2;
+    public static string Name {get;set;} = "Macaron";
 
     public Macaron() : base(Cost, Name)
     {
