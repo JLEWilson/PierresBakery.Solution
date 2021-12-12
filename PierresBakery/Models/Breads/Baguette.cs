964f08197ed35df9cc2063da44e973ebe8f28f69
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-12-12 11:54:49 -0900

    spelling and syntax fix

--- PierresBakery/Models/Breads/Baguette.cs
+++ PierresBakery/Models/Breads/Baguette.cs
@@ -5,7 +5,7 @@ namespace Bakery.Models
   public class Baguette : Bread
   {
     public static int Cost {get;set;} = 5;
-    public static string Name {get;set;} = "Croissant";
+    public static string Name {get;set;} = "Baguette";
 
     public Baguette() : base(Cost, Name)
     {
