d0e2f4e5b1aa7e8e7b01a74399acb92b8c7ceef5
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-12-12 08:32:48 -0900

    update class to contain name and better use OOP

--- PierresBakery/Models/Breads/SourDough.cs
+++ PierresBakery/Models/Breads/SourDough.cs
@@ -5,10 +5,10 @@ namespace Bakery.Models
   public class SourDough : Bread
   {
     public static int Cost {get;set;} = 8;
+    public static string Name {get;set;} = "SourDough";
 
-    public SourDough() : base(Cost)
+    public SourDough() : base(Cost, Name)
     {
-      this.BreadType = "Sourdough";
     }
   }
 }
