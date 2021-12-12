02ccf4739402332ea92c1ece70a4fafa5f3b292a
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-12-12 08:34:43 -0900

    updated objects to use new format, better for OOP

--- PierresBakery.Tests/ModelTests/FoodItemTests.cs
+++ PierresBakery.Tests/ModelTests/FoodItemTests.cs
@@ -16,34 +16,34 @@ namespace Bakery.Tests
     [TestMethod]
     public void FoodItemConstructor_CreateInstanceOfFoodItem_FoodItem()
     {
-      FoodItem newFoodItem = new FoodItem(3, "Generic");
+      FoodItem newFoodItem = new FoodItem(3, "Generic", "Generic");
       Assert.AreEqual(typeof(FoodItem), newFoodItem.GetType());
     }
 
     [TestMethod]
     public void GetPrice_ReturnPriceOfFoodItem_5()
     {
-      FoodItem newFoodItem = new FoodItem(5, "Generic");
+      FoodItem newFoodItem = new FoodItem(5, "Generic", "Generic");
       int price = newFoodItem.Price;
       Assert.AreEqual(5, price);
     }
     [TestMethod]
     public void CalculateTotal_ReturnPriceOfAllFoodItems_15()
     {
-      FoodItem newFoodItem = new FoodItem(5, "Generic");
-      FoodItem newFoodItem1 = new FoodItem(5, "Generic");
-      FoodItem newFoodItem2 = new FoodItem(5, "Generic");
+      FoodItem newFoodItem = new FoodItem(5, "Generic", "Generic");
+      FoodItem newFoodItem1 = new FoodItem(5, "Generic", "Generic");
+      FoodItem newFoodItem2 = new FoodItem(5, "Generic", "Generic");
       int total = FoodItem.CalculateTotal();
       Assert.AreEqual(15, total);
     }
     [TestMethod]
     public void ApplyDiscount_ReturnPriceOfAllItemsWithDiscount_20()
     {
-      FoodItem newFoodItem1 = new FoodItem(5, "Generic");
-      FoodItem newFoodItem2 = new FoodItem(5, "Generic");
-      FoodItem newFoodItem3 = new FoodItem(5, "Generic");
-      FoodItem newFoodItem4 = new FoodItem(5, "Generic");
-      FoodItem newFoodItem5 = new FoodItem(5, "Generic");
+      FoodItem newFoodItem1 = new FoodItem(5, "Generic", "Generic");
+      FoodItem newFoodItem2 = new FoodItem(5, "Generic", "Generic");
+      FoodItem newFoodItem3 = new FoodItem(5, "Generic", "Generic");
+      FoodItem newFoodItem4 = new FoodItem(5, "Generic", "Generic");
+      FoodItem newFoodItem5 = new FoodItem(5, "Generic", "Generic");
       int total = FoodItem.ApplyDiscount(5, 5); // Should reduce total by 5 for every 5 items ordered
       Assert.AreEqual(20, total);
     }
@@ -51,9 +51,9 @@ namespace Bakery.Tests
     public void AssignId_ReturnUniqueIdForEachFoodItem_Int()
     {
       //method is private, runs in the constructor
-      FoodItem newFoodItem1 = new FoodItem(5, "Generic");
-      FoodItem newFoodItem2 = new FoodItem(5, "Generic");
-      FoodItem newFoodItem3 = new FoodItem(5, "Generic");
+      FoodItem newFoodItem1 = new FoodItem(5, "Generic", "Generic");
+      FoodItem newFoodItem2 = new FoodItem(5, "Generic", "Generic");
+      FoodItem newFoodItem3 = new FoodItem(5, "Generic", "Generic");
       Assert.AreEqual(1, newFoodItem1.Id);
       Assert.AreEqual(2, newFoodItem2.Id);
       Assert.AreEqual(3, newFoodItem3.Id);
@@ -61,8 +61,8 @@ namespace Bakery.Tests
     [TestMethod]
     public void RemoveItemFromOrder_RemoveItemFromOrderList_False()
     {
-      FoodItem newFoodItem1 = new FoodItem(5, "Generic");
-      FoodItem newFoodItem2 = new FoodItem(5, "Generic");
+      FoodItem newFoodItem1 = new FoodItem(5, "Generic", "Generic");
+      FoodItem newFoodItem2 = new FoodItem(5, "Generic", "Generic");
       FoodItem.RemoveItemFromOrder(newFoodItem2.Id);
       bool doesContain = FoodItem.Order.Contains(newFoodItem2);
       Assert.AreEqual(false, doesContain);
